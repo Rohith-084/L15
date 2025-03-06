2db472758388ec47e4d0f41f918a8d900d4cc779
Rohith-084 <gali.kumar@s.amity.edu>
2025-03-06 11:43:54 +0530

    Replaced void main with int main and added a return statement at last.

--- add.c
+++ add.c
@@ -1,8 +1,9 @@
 #incluse<stdio.h>
-void main(){
+int main(){
 	int a=10,b=20;
 	printf("Sum is : %d",a+b);
 	printf("Difference is : %d",a-b);
 	printf("Product is : %d",a*b);
 	printf("Division is : %d",a/b);
+	return 0;
 }
